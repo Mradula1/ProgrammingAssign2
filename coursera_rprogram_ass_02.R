a2cb5a59a5398d2311f211d64946e9adbb634db1
Mradula1 <mradulasingh22@gmail.com>
2016-07-30 09:35:53 +0530

    Add files via upload

---
+++ coursera_rprogram_ass_02.R
@@ -0,0 +1,46 @@
+## Programming Assignment 02: Caching the Inverse of a Matrix
+
+## Matrix inversion is usually a costly computation and there may be some benefit to caching the inverse of a matrix rather than
+## computing it repeatedly.
+## makeCacheMatrix function creates a special "matrix" object that can cache its inverse.
+## Its purpose is to store a matrix and a cached value of the inverse of 
+# matrix. Following functions are used withen this function:
+# 1. setMatrix : sets the value of a matrix
+# 2. getMatrix : gets the value of a matrix
+# 3. setInverse : sets the value of inverse of the matrix
+# 4. getInverse : gets the value of inverse of the matrix
+
+
+makeCacheMatrix <- function(x = matrix()) {
+        InvMat <- NULL
+        set <- function(y) {
+                x <<- y
+                InvMat <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) InvMat <<- inverse
+        getInverse <- function() InvMat
+        list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+}
+## cacheSolve function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse
+## has already been calculated and the matrix has not changed, then cacheSolve function retrieves the inverse from the
+## cache.
+## if X is a square invertible matrix, then solve(X) function can be used to return its inverse.
+## Here we are assuming matrix to be invertible.
+
+
+cacheSolve <- function(x, ...) {
+# get the cached value
+        InvMat <- x$getInverse()
+# if a cached value exists return it
+        if (!is.null(InvMat)) {
+                message("getting cached data")
+                return(InvMat)
+        }
+# otherwise get the matrix, calculate the inverse of matrix and store it in the cache
+        mat <- x$get()
+        InvMat <- solve(mat, ...)
+        x$setInverse(InvMat)
+# returns the inverse
+        InvMat
+}
